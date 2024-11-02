fc4ac4a348a24f8c345993576d0426b935fdc3b8
sanjaymarathiiii <sanjaymarathiiii@gail.com>
2024-11-02 22:20:58 +0530

    The codeforces 3

---
+++ 2036A. Quintomania/main.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        
+        for(int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        bool isPerfect = true;
+        for(int i = 1; i < n; i++) {
+            int interval = abs(a[i] - a[i - 1]); 
+            if(interval != 5 && interval != 7) {
+                isPerfect = false;
+                break;
+            }
+        }
+
+        if(isPerfect) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
